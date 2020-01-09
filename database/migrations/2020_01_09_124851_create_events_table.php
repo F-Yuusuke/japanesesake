45d6c0d565fbed5d491a683a045b8666af100564
南部遼 <>
2020-01-10 00:25:42 +0800

    events テーブル用の migration ファイルを作成

---
+++ database/migrations/2020_01_09_124851_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30);
+            $table->text('description');
+            $table->dateTime('date');
+            $table->string('place');
+            $table->integer('price');
+            $table->integer('owner_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
