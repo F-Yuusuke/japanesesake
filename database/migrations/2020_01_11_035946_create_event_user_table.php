39a661d5455220d6b7b439c4200cb2d47287e10d
Ｆ.Ｙuusuke <>
2020-01-11 12:13:33 +0800

    event_userのファイルとテーブルを作成。

---
+++ database/migrations/2020_01_11_035946_create_event_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('Event_id');
+            $table->integer('User_id');
+            $table->integer('People_count');
+            $table->string('Special_comment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_user');
+    }
+}
