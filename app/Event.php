715d1aa3a8048af1dfeef12bad51c85efe9afa28
滝口結 <>
2020-01-20 15:23:20 +0900

    Revert "Revert "申込機能は完了しました。""
    This reverts commit 04e502935d2b529b8e4b5b2b1b0318fb990a967f.

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    public function event_user()
+    {
+        return $this->belongsToMany('App\userz', 'event_users')->withTimestamps();
+    }
 }
