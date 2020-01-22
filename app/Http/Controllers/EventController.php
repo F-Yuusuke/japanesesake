b04cf29810cfacab53addb631558d2768ad7d553
南部遼 <>
2020-01-22 16:02:52 +0800

    マージしたときにイベント参加申込み機能の一部を削除してしまったので修正です。

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Event;
+use App\Event_user;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -104,6 +105,31 @@ class EventController extends Controller
         $imgPath = $image->store('images/eventPicture', 'public');
 
         return 'storage/' . $imgPath;
+    }
+
+
+    public function event_apply(int $id)//申込
+    {
+        $event = Event::find($id);
+
+        // return redirect()->route('event.apply');
+
+        return view('events.apply', ['event' => $event]);
+    }
+    public function event_applyed(int $id, Request $request)//申込完了
+    {
+        $event_users = new Event_user();
+
+        $event_users->User_id = $request->id;
+        $event_users->Event_id = '000';//仮設id
+        $event_users->People_count = '1';
+        $event_users->Special_comment = $request->Special_comment;
+
+        // dd($event_user);
+        // $event_user = Event_user::where('id', $id)->with('event_user')->first();
+        $event_users->save();
+
+        return redirect()->route('event.index');
     }
 }
 
