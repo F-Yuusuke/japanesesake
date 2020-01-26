136dadec1761fcfd1bfb087cffcf4e1740ed987e
南部遼 <>
2020-01-26 19:43:55 +0800

    外国人ユーザーの参加申込み画面のデザイン

--- resources/views/events/apply.blade.php
+++ resources/views/events/apply.blade.php
@@ -5,6 +5,7 @@
 <section class="container m-5">
         <div class="row justify-content-center">
             <div class="col-8">
+                    <h1 class="text-center">Event Detail</h1>
                 @if($errors->any())
                     <ul>
                         @foreach($errors->all() as $message)
@@ -16,18 +17,18 @@
                     @csrf
                     @method('put')
                     <div class="form-group">
-                        <label for="name">Event Title : "{{ old('name', $event->name) }}"</label>
+                        <label for="name">Event Title : {{ old('name', $event->name) }}</label>
                     </div>
                     <div class="form-group">
-                        <label for="date">Date : "{{ old('date', $event->date) }}"</label>
+                        <label for="date">Date : {{ old('date', $event->date) }}</label>
                     </div>
                     <div class="form-group">
-                        <label for="title">Place :  "{{ old('place', $event->place) }}"</label>
+                        <label for="place">Place :  {{ old('place', $event->place) }}</label>
                     </div>
                     <div class="form-group">
-                        <label for="price">Price :  "{{ old('price', $event->price) }}"</label>
+                        <label for="price">Price :  {{ old('price', $event->price) }} 円</label>
                     </div>
-                    <div class="form-group">
+                    <div class="request">
                         <label for="body">Let us know if you need special help.</label>
                         <textarea class="form-control" name="Special_comment" id="Special_comment">{{ old('Special_comment') }}</textarea>
                     </div>
@@ -36,8 +37,8 @@
                     <input type="hidden" class="form-control" name="eventid" id="eventid" value="{{ old('id', $event->id) }}">
                     {{-- ここにuseridを書いたらいいんじゃないかな --}}
 
-                    <div class="text-right">
-                        <button type="submit" class="btn btn-primary">Book Now</button>
+                    <div class="text-center">
+                        <button type="submit" class="btn mt-5">Book Now</button>
                     </div>
                 </form>
             </div>
