140e6e82ddafc551c947da7c9dab79dc7d688bcb
南部遼 <>
2020-01-26 17:47:39 +0800

    layout を 外国人ユーザー側(app.blade.php)と酒蔵側(app_owner.blade.php)に分ける

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,7 +47,7 @@
                           <a class="nav-link" href="/event">Events List</a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="/login">Login</a>
+                          <a class="nav-link" href="/mypage">Mypage</a>
                         </li>
                       </ul>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/event">Event</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/mypage">Mypage</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -49,27 +52,27 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('owner.login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('owner.register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::guard('owner')->user()->name }} <span class="caret"></span>
+                                    {{ Auth::guard('user')->user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('owner.logout') }}"
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('ログアウト') }}
+                                        {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('owner.logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
