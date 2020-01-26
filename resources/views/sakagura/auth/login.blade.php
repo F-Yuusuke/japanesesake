8ee46d2b27cf157148f0c80bbb7b7c68d23c9a15
Ｆ.Ｙuusuke <>
2020-01-26 14:01:21 +0800

    酒蔵側のlogin画面作成 ログインしていない人は新規登録ページを作成

--- resources/views/sakagura/auth/login.blade.php
+++ resources/views/sakagura/auth/login.blade.php
@@ -53,13 +53,17 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-dark">
                                     {{ __('ログイン') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('パスワードをお忘れですか?') }}
+                                    </a>
+                                    <!-- ここをクリックされたら新規登録ページに飛ばすようにする -->
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('登録がまだの人はこちらから新規登録') }}
                                     </a>
                                 @endif
                             </div>
