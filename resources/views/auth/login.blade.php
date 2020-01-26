6d98b78f1740aeacbe4dd4d54ec66d8f3aad8456
Ｆ.Ｙuusuke <>
2020-01-26 16:15:23 +0800

    登録がまだの人は新規登録してくださいリンクを作った ボタンの色を変更 リンク先に飛ばすことができていない 上に出てくるlalavelのナビを消すことができていない

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
+<!-- {{--  --}} laravelのコメントアウト -->
+{{--@section('content') --}}
 
-@section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -53,13 +55,17 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit btn btn-dark" class="btn btn-dark">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
+                                    </a>
+                                    <!-- ここもリンクだけ作ったけどどこに飛ぶかは書いていない -->
+                                    <a class="btn btn-link " href="{{ route('password.request') }}">
+                                        {{ __('If you have not registered yet, register here') }}
                                     </a>
                                 @endif
                             </div>
@@ -70,4 +76,6 @@
         </div>
     </div>
 </div>
-@endsection
+
+	
+{{-- @endsection --}}
