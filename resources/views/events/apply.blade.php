cbf0ebb4c4fcd4e2963696973580cfc79aa4f232
南部遼 <>
2020-01-27 08:41:26 +0800

    no message

--- resources/views/events/apply.blade.php
+++ resources/views/events/apply.blade.php
@@ -16,16 +16,16 @@
                 <form action="{{ route('event.applyed', ['id' => $event->id]) }}" method="post">
                     @csrf
                     @method('put')
-                    <div class="form-group">
+                    <div class="apply-form">
                         <label for="name">Event Title : {{ old('name', $event->name) }}</label>
                     </div>
-                    <div class="form-group">
+                    <div class="apply-form">
                         <label for="date">Date : {{ old('date', $event->date) }}</label>
                     </div>
-                    <div class="form-group">
+                    <div class="apply-form">
                         <label for="place">Place :  {{ old('place', $event->place) }}</label>
                     </div>
-                    <div class="form-group">
+                    <div class="apply-form">
                         <label for="price">Price :  {{ old('price', $event->price) }} 円</label>
                     </div>
                     <div class="request">
