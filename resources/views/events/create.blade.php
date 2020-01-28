197340dccab7d02e63b335347aa379b9577bcd81
Atsushi <>
2020-01-28 17:52:54 +0800

    いらんコメント削除

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -13,41 +13,26 @@
                 <h1 class="text-center">新規イベントの登録</h1>
                 <form action="{{ route('event.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
-                    <!-- イベント名 -->
                     <div class="form-group">
                         <label for="title">イベント名</label>
                         <input type="text" class="form-control" name="name" id="name" />
                     </div>
-                    <!-- 詳細 -->
                     <div class="form-group">
                         <label for="body">イベントの詳細情報</label>
                         <textarea class="form-control" name="description" id="description"></textarea>
                     </div>
-                    <!-- 日付 -->
                     <div class="form-group">
                         <label for="title">日時</label>
                         <input type="text" class="form-control" name="date" id="date" />
                     </div>
-                    <!-- 開催地 -->
                     <div class="form-group">
                         <label for="title">会場</label>
                         <input type="text" class="form-control" name="place" id="place" />
                     </div>
-                    <!-- 値段 -->
                     <div class="form-group">
                         <label for="title">値段</label>
                         <input type="text" class="form-control" name="price" id="price" />
                     </div>
-                    <!-- オーナーID -->
-                    {{-- <div class="form-group">
-                        <label for="title">owner_id</label>
-                        <input type="text" class="form-control" name="owner_id" id="owner_id" />
-                    </div> --}}
-                    <!-- 画像 -->
-                    <!-- <div class="form-group">
-                        <label for="title">image</label>
-                        <input type="text" class="form-control" name="picture_path" id="picture_path" />
-                    </div> -->
                     <div class="form-group row">
                         <label for="picture" class="col-form-label text-md-right">イメージ画像</label>
 
@@ -59,7 +44,6 @@
                                 </span>
                             @endif
                     </div>
-                    <!-- ボタン -->
                     <div class="text-center">
                         <button type="submit" class="btn_go">投稿</button>
                     </div>
