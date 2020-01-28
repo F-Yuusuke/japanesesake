bca980ef4ee004242d1e29c76d76e846411f4b7d
Atsushi <>
2020-01-28 16:59:40 +0800

    データの取り方改善and不要なevent一覧削除

--- app/Http/Controllers/Owner/OwnerController.php
+++ app/Http/Controllers/Owner/OwnerController.php
@@ -3,23 +3,16 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
-use App\Owner;
 use \Auth;
-use App\Event;
 
 
 class OwnerController extends Controller
 {
     public function mypage()
     {
-        $owner = Owner::find(Auth::guard('owner')->user()->id);
-        // $events = Event::all();
-        $events = Event::where("owner_id", Auth::guard('owner')->user()->id)->get();
+        $owner = Auth::user();
+        $owner->load('events');
 
-        return view('owner.mypage',[
-            'owner' => $owner,
-            'events' => $events,
-        ]);
+        return view('owner.mypage',['owner' => $owner,]);
     }
-
 }
--- resources/views/owner/mypage.blade.php
+++ resources/views/owner/mypage.blade.php
@@ -7,7 +7,7 @@
 
             <div class="container d-flex justify-content-between">
               <h1>あなたの酒蔵の登録情報</h1>
-            </div> 
+            </div>
 
             {{-- ログインした酒蔵の情報表示 --}}
             <div class='container'>
@@ -24,47 +24,15 @@
                  </div>
                 {{-- @endforeach --}}
             </div>
-
-
-  <div class="container">
-  @foreach ($events as $event)
-  <div class="m-4 p-4 border box10">
-    <div class="row no-gutters ">
-      <div class="col-md-4 rounded">
-      <svg class="bd-placeholder-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image"><title>ああああああ</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
-      </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h1 class="card-title">{{ $owner->name }}</h1>
-            <p class="card-text">{{ $owner->zipcode }}</p>
-            <p class="card-text">{{ $owner->address }}</p>
-            <p class="card-text">{{ $owner->email }}</p>
-            <p class="card-text">{{ $owner->tel }}</p>
-            <p class="card-text">{{ $owner->description }}</p>
-            <p class="card-text">{{ $owner->picture_path }}</p>
-
-          </div>
-       </div>
-    </div>
-  </div>
- @endforeach
-  </div> 
-
-
-
-
-
-            
-
             <div class="container d-flex justify-content-between">
               <h1>あなたの酒蔵のイベント情報</h1>
-            </div> 
+            </div>
 
         <div class="container">
 
            <div class="row no-gutters">
                 {{-- ログインした酒蔵のイベント表示 --}}
-                @foreach ($events as $event)
+                @foreach ($owner->events as $event)
                 <div class="m-4 p-4  border box10">
                   <div class="col-mb-4 rounded">
                   <svg class="bd-placeholder-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image"><title>ああああああ</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
@@ -96,6 +64,4 @@
         </div>
     </div>
 </div>
-
-
-@endsection—
+@endsection
