29051625edcb183388079702fd9109e15836d3d2
Ｆ.Ｙuusuke <>
2020-01-28 22:14:55 +0800

    外国人ユーザーのマイページを調整

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -4,30 +4,26 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Mypage</div>
 
-            </div>
-
-            <h1>User Information</h1>
             {{-- ログインしたユーザーの情報表示 --}}
             <div class="container">
-                <div class="m-4 p-4 border box10">
+            <h2 class='text-center'>User Information</h2>
+                <div class="m-4 p-4 border border-primary">
                 {{-- @foreach ($users as $users) --}}
-                        <h1>{{ $user->name }}</h1>
-                        <p>{{ $user->county_id }}</p>
-                        <p>{{ $user->sex}}</p>
-                        <p>{{ $user->email}}</p>
-                        <p>{{ $user->birthday}}</p>
+                        <h1 class='card-title'>{{ $user->name }}</h1>
+                        <p class='card-text'>{{ $user->county_id }}</p>
+                        <p class='card-text'>{{ $user->sex}}</p>
+                        <p class='card-text'>{{ $user->email}}</p>
+                        <p class='card-text'>{{ $user->birthday}}</p>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                         {{-- <p>{{ $hoge }}</p> --}}
                     {{-- @endforeach --}}
                 </div>
             </div>
 
-            <h1>Your Booking</h1>
             {{-- ログインしたユーザーが申し込んでいるイベント表示 --}}
             <div class="container">
+            <h1 class='text-center my-2'>Your Booking</h1>
                 @foreach ($events as $event)
                     <div class="m-4 p-4 border box10">
                         <div class="row no-gutters ">
@@ -38,18 +34,29 @@
                             <div class="card-body">
                                 <h5 class="card-title">{{ $event->event->name }}</h5>
                                 <p class="card-text">{{ $event->event->description }}</p>
-                                <p class="card-text"><small class="text-muted">{{ $event->event->date }}</small></p>
-                                <p class="card-text">{{ $event->event->place }}</p>
-                                <p class="card-text">{{ $event->event->price }}</p>
+                                <p class="card-text">
+                                <small class="text-muted">{{ $event->event->date }}</small>
+                                </p>
+                              <div class='d-flex'>
+                                <p class="card-text mr-5">
+                                <small class="text-muted">Place：</small>
+                                {{ $event->event->place }}
+                                </p>
+                                <p class="card-text">
+                                <small class="text-muted">Price：</small>
+                                {{ $event->event->price }}JPY
+                                </p>
+                              </div>  
                             </div>
                             {{-- @if (Auth::check() && Auth::user()->id === $event->user_id) --}}
+                            <div class='d-flex justify-content-end'>
                                 <form action="{{ route('event.cancel', ['id' => $event->id]) }}" method="post" class="d-inline">
                                 @csrf
                                 @method('delete')
-                                <button class="btn btn-danger">event cancel</button>
-
+                                <button class="btn btn-danger ml-2">event cancel</button>
                                 </form>
-                            {{-- @endif --}}
+                            </div>
+                            {{-- @endif --}} 
                         </div>
                         </div>
                     </div>
