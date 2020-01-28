be09ba1b7f68826fbfa2afc3fe33ae2c2d899a4f
Ｆ.Ｙuusuke <>
2020-01-28 15:32:49 +0800

    no message

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -11,38 +11,47 @@
 
             <h1>User Information</h1>
             {{-- ログインしたユーザーの情報表示 --}}
-            <div>
-                 <div class="m-4 p-4 border border-primary">
-                 <div class="row no-gutters">
-                        <div class="col-md-4">
-                        <svg class="bd-placeholder-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
-                        </div>
-                           <h5 class="card-title">{{ $user->name }}</h5>
-                            <p class="card-text">{{ $user->country_id }}</p>
-                            <p class="card-text">{{ $user->sex }}</p>
-                            <p class="card-text">{{ $user->email }}</p>
-                            <p class="card-text">{{ $user->birthday }}</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                 </div>
+            <div class="container">
+                <div class="m-4 p-4 border box10">
+                {{-- @foreach ($users as $users) --}}
+                        <h1>{{ $user->name }}</h1>
+                        <p>{{ $user->county_id }}</p>
+                        <p>{{ $user->sex}}</p>
+                        <p>{{ $user->email}}</p>
+                        <p>{{ $user->birthday}}</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        {{-- <p>{{ $hoge }}</p> --}}
+                    {{-- @endforeach --}}
+                </div>
             </div>
 
+
+
+
+
+
             <h1>Your Booking</h1>
-            {{-- ログインしたユーザーが申し込んでいるイベント表示 --}}
-            @foreach ($events as $event)
-            <div class="m-4 p-4 border border-light">
-                <h1>{{ $event->event->name }}</h1>
-                <p>{{ $event->event->description }}</p>
-                <p>{{ $event->event->date }}</p>
-                <p>{{ $event->event->place }}</p>
-                <p>{{ $event->event->price }}</p>
-                <img height="100px" src="{{ $event->picture_path }}" >
-            </div>
-            @endforeach
-        </div>
-    </div>
-</div>
-@endsection
+            {{-- ログインしたユーザーが申し込んでいるイベント表示 イベント一覧ページからコピペ--}}
+            <div class="container">
+                @foreach ($events as $event)
+                    <div class="m-4 p-4 border box10">
+                        <div class="row no-gutters ">
+                        <div class="col-md-4 rounded">
+                        <svg class="bd-placeholder-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image"><title>ああああああ</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
+                        </div>
+                            <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $event->name }}</h5>
+                                <p class="card-text">{{ $event->description }}</p>
+                                <p class="card-text"><small class="text-muted">{{ $event->date }}</small></p>
+                                <p class="card-text">{{ $event->place }}</p>
+                                <p class="card-text">{{ $event->price }}</p>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div> 
 
 
 
