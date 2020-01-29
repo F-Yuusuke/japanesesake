e5283574ec49b512a67653e2dac12d6c283dbe0d
Atsushi <>
2020-01-29 14:26:12 +0800

    データの取り方修正

--- app/User.php
+++ app/User.php
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function goingEvents()
+    {
+        return $this->belongsToMany('App\Event', 'event_users')->withTimestamps();
+    }
 }
